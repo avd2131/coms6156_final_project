ee750be1c037dde8b798dbb64a23c968392728c6
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-02-22 15:30:43 -0500

    Scroll functionality maintained when window is resized

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,9 +1,6 @@
 export let verticalScrollStatus = 'top';
 
-if (window.innerHeight === document.body.scrollHeight) verticalScrollStatus = 'noscroll';
-
-// Detects if at the top/bottom of screen when scrolling
-document.addEventListener('scroll', (e) => {
+function updateScrollStatus() {
 	if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
 		// At bottom of page
 		verticalScrollStatus = 'bottom';
@@ -11,5 +8,18 @@ document.addEventListener('scroll', (e) => {
 
 	if (window.scrollY === 0) verticalScrollStatus = 'top';
 
+	if (window.innerHeight === document.body.scrollHeight) verticalScrollStatus = 'noscroll';
+
 	// console.log('Scrolled. Scroll status:', verticalScrollStatus);
+}
+
+updateScrollStatus();
+
+// Detects if at the top/bottom of screen when scrolling
+document.addEventListener('scroll', (e) => {
+	updateScrollStatus();
+});
+
+window.addEventListener('resize', () => {
+	updateScrollStatus();
 });
